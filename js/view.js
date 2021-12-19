cab8f9933dda6a3c99f538acc2b45c4a85fd8611
Artem1yMarenkov <orvysegor@gmail.com>
2021-12-19 13:50:00 +0300

    Refactored view.js

--- js/view.js
+++ js/view.js
@@ -1,13 +1,117 @@
-const UIComponents = {
+import API from "./API.js";
+
+const UI = {
     tabs: document.querySelectorAll('.tab'),
     slides: document.querySelectorAll('.slide'),
     activeTab: document.querySelector('.tab._active'),
     form: document.querySelector('.search-form'),
-    cityNameField: document.querySelectorAll('.city-name'),
+    cityNameFields: document.querySelectorAll('.city-name'),
     tempField: document.querySelector('.temperature'),
     weatherIcon: document.querySelector('.weather-icon'),
     addFavouriteBtn: document.querySelector('.add-favourite'),
     favouritesList: document.querySelector('.favourites'),
+    favouriteItems: [...document.querySelectorAll('.favourite-item')],
 }
 
-export default UIComponents;
+const ADDED_LOCATIONS = ['Kirov', 'Kazan’'];
+
+
+function checkHeart() {
+    const favouriteItems = [...document.querySelectorAll('.favourite-item')];
+    const iterations = favouriteItems.map(el => el.childNodes[1].textContent);
+    const cityName = UI.cityNameFields[0].textContent;
+
+    iterations.includes(cityName) 
+        ? UI.addFavouriteBtn.className = 'add-favourite _active'
+        : UI.addFavouriteBtn.className = 'add-favourite'
+}
+
+
+function setHTML(data) {
+    UI.cityNameFields.forEach(el => el.innerHTML = data["name"]);
+    UI.tempField.innerHTML = `${Math.round(data['main']['temp'])}°`;
+    UI.weatherIcon.src = `https://openweathermap.org/img/wn/${data['weather'][0]['icon']}@2x.png`;
+
+    checkHeart();
+}
+
+
+function setTab(event) {
+    const slideId = event.target.getAttribute('data-action');
+
+    UI.slides.forEach(element => {
+        if (element.classList.contains('_active')) element.classList.remove('_active');
+
+        if (element.getAttribute('data-action') === slideId) element.classList.add('_active');
+    });
+
+    UI.activeTab.classList.remove('_active');
+    event.target.classList.add('_active');
+    UI.activeTab = event.target;
+}
+
+
+function renderFavourites() {
+    UI.favouritesList.innerHTML = '';
+    
+    ADDED_LOCATIONS.forEach(city => {
+        const favouriteItem = document.createElement('li');
+
+        favouriteItem.className = 'favourite-item';
+        favouriteItem.innerHTML = `
+                <button class="favourite-city-name" >${city}</button>
+                <button class="favourite-cross">+</button>
+            `;
+        favouriteItem.addEventListener('click', handleClick);
+
+        UI.favouritesList.appendChild(favouriteItem);
+    });
+}
+
+
+function addFavourite() {
+    const cityName = UI.cityNameFields[0].textContent;
+    const replys = ADDED_LOCATIONS.includes(cityName);
+
+    if (!replys) {
+        ADDED_LOCATIONS.push(cityName);
+        renderFavourites();
+    }
+
+    checkHeart();
+}
+
+
+function removeFavourite(element) {
+    const cityName = element.parentNode.childNodes[1].textContent;
+    const index = ADDED_LOCATIONS.indexOf(cityName);
+    ADDED_LOCATIONS.splice(index, 1);
+
+    renderFavourites();
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const cityName = event.target.city.value.trim();
+    API.sendRequest(cityName, setHTML);
+
+    event.target.reset();
+}
+
+function handleClick(event) {
+    if (event.target.className == 'favourite-cross') {
+        removeFavourite(event.target);
+    }
+    if (event.target.className == 'favourite-city-name') {
+        const cityName = event.target.textContent;
+        API.sendRequest(cityName, setHTML);
+    }
+    checkHeart();
+}
+
+UI.addFavouriteBtn.addEventListener('click', addFavourite);
+UI.tabs.forEach(el => el.addEventListener('click', setTab));
+
+document.addEventListener('DOMContentLoaded', renderFavourites);
+UI.form.addEventListener('submit', handleSubmit);
